42f0c4e829d7a3a8b3d4bda2238ddc5d8f32563c
Iza Cabral <izabelecabral@hotmail.com>
2024-11-04 16:47:56 -0300

    feat: install and instantiate fastify

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,19 @@
-const myName: string = "Iza"
+import Fastify from 'fastify'
 
-console.log(myName)
+const server = Fastify()
+
+server.get("/", async (req, res) => {
+	res.type("application/json").code(200)
+	return {hello: "world"}
+})
+
+const start = async () => {
+	try {
+		await server.listen({ port: 3030 })
+		console.log ("Server running on https://localhost:3030")
+	} catch (error) {
+		server.log.error(error),
+		process.exit(1)
+	}
+}
+start()
