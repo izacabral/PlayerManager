170ebf0de43cf5096ba780986b71c5a73605709f
Iza Cabral <izabelecabral@hotmail.com>
2024-11-06 13:30:05 -0300

    chore: run lint

--- src/schemas/skillsSchema.ts
+++ src/schemas/skillsSchema.ts
@@ -3,6 +3,10 @@ import { z } from 'zod';
 export const skillsSchema = z.object({
   id: z.string().uuid().optional(),
   strength: z.number().int().min(0).max(10),
-  speed: z.number().int().min(0, { message: "A velocidade não pode ser menor que 0." }).max(10),
+  speed: z
+    .number()
+    .int()
+    .min(0, { message: 'A velocidade não pode ser menor que 0.' })
+    .max(10),
   driblle: z.number().int().min(0).max(10).optional(),
 });
